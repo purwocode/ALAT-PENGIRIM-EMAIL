3f4b63dcf92dd464057c3396c0dd4fa7461b1077
Zad Bagus <zadbagus7@gmail.com>
2024-09-03 11:00:41 +0700

    Update smtp_config.php

--- smtp_config.php
+++ smtp_config.php
@@ -4,15 +4,15 @@ return [
         'smtp_host' => 'smtp-relay.gmail.com',
         'smtp_port' => 587,
         'smtp_encryption' => 'tls',
-        'smtp_username' => 'aowkaofinf@freesignalbinary.com',
-        'smtp_password' => 'wrry hjdh mdcv dalo',
+        'smtp_username' => 'example@example.com',
+        'smtp_password' => 'example',
     ],
     [
         'smtp_host' => 'smtp-relay.gmail.com',
         'smtp_port' => 587,
         'smtp_encryption' => 'tls',
-        'smtp_username' => 'aowkaofinf2@freesignalbinary.com',
-        'smtp_password' => 'zxcasdqwe321',
+        'smtp_username' => 'example@example.com',
+        'smtp_password' => 'example',
     ],
 
     // Tambahkan konfigurasi SMTP lainnya jika diperlukan
